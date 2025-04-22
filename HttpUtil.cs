71ee27a2dd87a468432637cbe66482c7b6aadc76
XiaoShuaiRKK <111045365+XiaoShuaiRKK@users.noreply.github.com>
2025-04-23 07:59:57 +0800

    Add files via upload

---
+++ HttpUtil.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session1Winform.Model
+{
+    public class HttpUtil
+    {
+        private static HttpClient client = new HttpClient();
+        public static HttpClient GetHttp()
+        {
+            return client;
+        }
+        public static void SetToken(string token)
+        {
+            client.DefaultRequestHeaders.Authorization 
+                = new AuthenticationHeaderValue("Bearer", token);
+        }
+    }
+}
---
+++ Token.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session1Winform.Model
+{
+    public class Token
+    {
+        public string access_token { get; set; }
+        public string session_id { get; set; }
+    }
+}
