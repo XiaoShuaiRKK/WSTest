07d8c9042eeda312112a79411fd5ddb482003228
XiaoShuaiRKK <111045365+XiaoShuaiRKK@users.noreply.github.com>
2025-04-23 08:04:59 +0800

    Add files via upload

---
+++ ProductsController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using NuGet.Common;
+using Session1WebApi.Data;
+using Session1WebApi.Model;
+
+namespace Session1WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/products")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly AppDbContext context;
+        public ProductsController(AppDbContext context)
+        {
+            this.context = context;
+        }
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var authHeader = Request.Headers["Authorization"].ToString();
+            if(authHeader.IsNullOrEmpty() && !authHeader.StartsWith("Bearer "))
+            {
+                return Unauthorized(ErrorMessage.Unauthorized());
+            }
+            authHeader = authHeader.Substring("Bearer ".Length);
+            if (!TokenTool.CheckToken(authHeader))
+            {
+                return Unauthorized(ErrorMessage.Unauthorized());
+            }
+            return Ok(context.Products.Include(p => p.Supplier).ToList());
+        }
+
+        [HttpPut]
+        [Route("{id}/stock")]
+        public IActionResult UpdateStock(int id, [FromQuery]int stock)
+        {
+            var authHeader = Request.Headers["Authorization"].ToString();
+            if (String.IsNullOrEmpty(authHeader) || !authHeader.StartsWith("Bearer "))
+            {
+                return Unauthorized(ErrorMessage.Unauthorized());
+            }
+            authHeader = authHeader.Substring("Bearer ".Length);
+            if (!TokenTool.CheckToken(authHeader))
+            {
+                return Unauthorized(ErrorMessage.Unauthorized());
+            }
+
+
+            var product = context.Products.Find(id);
+            if(product == null)
+            {
+                return Unauthorized(ErrorMessage.Unauthorized());
+            }
+            product.Stock = stock;
+            return Ok(context.Products.Update(product));
+        }
+    }
+}
---
+++ TokenTool.cs
@@ -0,0 +1,27 @@
+﻿namespace Session1WebApi.Data
+{
+    public class TokenTool
+    {
+        public static Dictionary<string, string> tokenStore = new Dictionary<string, string>();
+        public static Dictionary<string, string> sessionStore = new Dictionary<string, string>();
+
+        public static string CreateToken(string email)
+        {
+            var token = Guid.NewGuid().ToString();
+            tokenStore[token] = email;
+            return token;
+        }
+
+        public static bool CheckToken(string token)
+        {
+            return tokenStore.ContainsKey(token);
+        }
+
+        public static string CreateSessionId(string email)
+        {
+            var seesionId = Guid.NewGuid().ToString();
+            sessionStore[seesionId] = email;
+            return seesionId;
+        }
+    }
+}
